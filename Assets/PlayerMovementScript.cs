6d25885aa38759d91e14c7be98171455a6aeb481
Chilmaqwe <mahreenzarah21@gmail.com>
2026-01-24 10:36:08 -0500

    harpoon throw script

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool fish;
+
+    [Header("General movement")]
+    public Vector3 moveDirection;
+    public float forceScalar;
+    public float rotationSpeed;
+    [Header("Stroke settings")]
+    public float strokeForce;
+    public float forceTimer;
+
+    public GameObject harpoon;
+    public HarpoonScript harpoonPrefab;
+    private HarpoonScript spawnedHarpoon;
+    public Vector3 hStartPos;
+    public float hForce;
+    public bool thrown = false;
+
+    void Start()
+    {
+        fish = false;
+        hStartPos = harpoon.GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadMovementInput();
+        ApplyMovement();
+        ApplyRotation();
+        ReadStrokeInput();
+
+        // attack
+        if (!fish)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                HarpoonAttack();
+            }
+
+        }
+    }
+
+    void ReadMovementInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector3(x, 0, z);
+    }
+
+    void ApplyMovement()
+    {
+        Vector3 finalDirection;
+        if (!(moveDirection == Vector3.zero) && Vector3.Magnitude(GetComponent<Rigidbody>().velocity) < 10)
+        {
+
+            finalDirection = Camera.main.transform.right * moveDirection.x + Camera.main.transform.forward * moveDirection.z;
+
+            finalDirection.Normalize();
+
+            GetComponent<Rigidbody>().AddForce(forceScalar * finalDirection, ForceMode.Impulse);
+
+
+        }
+
+
+    }
+
+    void ApplyRotation()
+    {
+        Quaternion targetRotation = Camera.main.transform.rotation;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+
+
+    void ReadStrokeInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && forceTimer <= 0)
+        {
+            ApplyStroke();
+            forceTimer = 0.5f;
+        }
+        else if (forceTimer > 0)
+        {
+            forceTimer -= Time.deltaTime;
+        }
+    }
+
+    void ApplyStroke()
+    {
+        GetComponent<Rigidbody>().AddForce(strokeForce * Camera.main.transform.forward, ForceMode.Impulse);
+    }
+
+    void HarpoonAttack()
+    {
+        if (spawnedHarpoon != null)
+        {
+            return;
+        }
+
+        spawnedHarpoon = Instantiate(harpoonPrefab, harpoon.transform.position, harpoon.transform.rotation);
+        spawnedHarpoon.Throw(harpoon, hForce, 20, Camera.main.transform.forward);
+        harpoon.gameObject.SetActive(false);
+    }
+}
